58ad6264b235c7bd20ca01893ba0925b6165ca04
Tyler Roscoe <tyler@NOREPLY>
2023-06-09 12:31:12 -0600

    books Generalize to handle Hugo article as well

--- wikipedia_books.py
+++ wikipedia_books.py
@@ -4,8 +4,22 @@
 import pandas as pd
 
 
-TARGET_TABLE_COLUMNS = ("Year", "Author", "Novel")
-NEBULA_NOVELS_URL = "https://en.wikipedia.org/wiki/Nebula_Award_for_Best_Novel"
+ARTICLES = [
+    {
+        "author_column": "Author",
+        "award": "Nebula",
+        "category": "Novel",
+        "target_table_columns": ("Year", "Author", "Novel"),
+        "url": "https://en.wikipedia.org/wiki/Nebula_Award_for_Best_Novel",
+    },
+    {
+        "author_column": "Author(s)",
+        "award": "Hugo",
+        "category": "Novel",
+        "target_table_columns": ("Year", "Author(s)", "Novel"),
+        "url": "https://en.wikipedia.org/wiki/Hugo_Award_for_Best_Novel",
+    },
+]
 
 
 def fetch_url(url):
@@ -14,10 +28,10 @@ def fetch_url(url):
     return tables
 
 
-def find_target_table(tables):
+def find_target_table(tables, article):
     target_table = None
     for table in tables:
-        if all([column in table.columns for column in TARGET_TABLE_COLUMNS]):
+        if all([column in table.columns for column in article["target_table_columns"]]):
             target_table = table
             break
     else:
@@ -25,9 +39,9 @@ def find_target_table(tables):
     return target_table
 
 
-def drop_unwanted_columns(table):
+def drop_unwanted_columns(table, article):
     for column in table.columns:
-        if column not in TARGET_TABLE_COLUMNS:
+        if column not in article["target_table_columns"]:
             print(f"Dropping column {column}")
             table = table.drop(column, axis=1)
     return table
@@ -35,33 +49,33 @@ def drop_unwanted_columns(table):
 
 def main():
     books = []
-    for url in (NEBULA_NOVELS_URL,):
-        tables = fetch_url(url)
-        table = find_target_table(tables)
-        table = drop_unwanted_columns(table)
+    for article in ARTICLES:
+        tables = fetch_url(article["url"])
+        table = find_target_table(tables, article)
+        table = drop_unwanted_columns(table, article)
         num_rows = table.shape[0]
 
         # Populate new columns
-        award_col = ["Nebula" for _ in range(num_rows)]
+        award_col = [article["award"] for _ in range(num_rows)]
         table = table.assign(Award=award_col)
 
-        category_col = ["Novel" for _ in range(num_rows)]
+        category_col = [article["category"] for _ in range(num_rows)]
         table = table.assign(Category=category_col)
 
         winner_col = []
         for row in table.iterrows():
-            if row[1]["Author"].endswith("*"):
+            if row[1][article["author_column"]].endswith("*"):
                 winner_col.append("Winner")
                 # Also modify Author to remove marker
-                table.at[row[0], "Author"] = table.at[row[0], "Author"][:-1]
+                table.at[row[0], article["author_column"]] = table.at[row[0], article["author_column"]][:-1]
             else:
                 winner_col.append("Nominee")
         table = table.assign(Winner=winner_col)
 
 
         # Debugggggg printffffffff
-        #print("\n".join([f"({row[1]['Year']}) {row[1]['Author']} - {row[1]['Novel']}" for row in table.iterrows() if row[1]["Winner"] != "Winner"]))
-        print(table)
+        #print("\n".join([f"({row[1]['Year']}) {row[1][article["author_column"]]} - {row[1]['Novel']}" for row in table.iterrows() if row[1]["Winner"] != "Winner"]))
+        print(table.to_csv())
 
 
 if __name__ == "__main__":
